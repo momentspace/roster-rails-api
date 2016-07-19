66aaa320fc56500cd0c36e525b274296e769bb06
momentspace <service@starferry.net>
2016-07-19 20:41:23 +0900

    Time型の設定追加

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,9 @@ module RosterRailsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # timezone setting
+    config.time_zone = 'Tokyo'
+    config.active_record.time_zone_aware_types = [:datetime, :time]
   end
 end
